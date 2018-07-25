9f4302cc35d0c33772fe78efa7daaa4c412f2967
Nick Sanford <nicholascsanford@gmail.com>
2018-07-25 17:22:38 -0400

    Get tests passing

--- htorrent/src/BEncode.hs
+++ htorrent/src/BEncode.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 module BEncode where
 import qualified Data.Map as M
-import Data.Char (digitToInt)
 import Data.Maybe (isNothing, fromJust)
 import Data.List (foldl', unfoldr)
 
@@ -18,9 +17,6 @@ data Run a = Run UnparsedContent (Maybe a) deriving (Eq, Show)
 encode :: BEncode -> String
 encode = undefined
 
---takeDrop :: (Char -> Bool) -> (String, String)
---takeDrop f xs = (takeWhile f xs, dropWhile f xs)
-
 digitToI :: Char -> Maybe Int
 digitToI '0' = Just 0
 digitToI '1' = Just 1
@@ -34,40 +30,10 @@ digitToI '8' = Just 8
 digitToI '9' = Just 9
 digitToI _   = Nothing
 
-bsfold :: (Char -> b -> b) -> b -> String -> b
-bsfold f z ([]) = z
-bsfold f z (x:xs) = f x $ bsfold f z xs
-
--- parse :: Char -> (String, String, BEncode)
--- parse 'd' (bsBuffer, refference, BEmpty) = ('d':bsBuffer, refference, BDict (M.fromList []))
--- parse 'd' (bsBuffer, refference, bencodeAcc) = ('d':bsBuffer, bencodeAcc)
--- parse 'i' (bsBuffer, refference, bencodeAcc)
--- parse 'l' (bsBuffer, refference, bencodeAcc)
--- parse _   (bsBuffer, refference, bencodeAcc)
---("", BEmpty)
-
---   | BS.uncons(bytes) == Just ('d', rest) = takeDrop (/= ':') rest
---   | BS.uncons(bytes) == Just ('i', rest) =
---   | BS.uncons(bytes) == Just ('l', rest) =
---   | otherwise =
-
--- test :: String -> IO (String)
--- test filePath = do
---   fmap decode $ readFile filePath
 test :: String -> IO (Run BEncode)
 test filePath = do
   xs <- readFile filePath
   return $ decode xs
-  -- let flag = letterToEmpty x
-  -- if isNothing flag
-  -- then return Nothing
-  -- else return $ decode xs (fromJust flag)
-
--- data BEncode = BInteger Integer
---              | BString String
---              | BList [BEncode]
---              | BDict (M.Map BEncode BEncode)
---              deriving (Eq, Show, Ord)
 
 letterToEmpty :: Char -> Maybe BEncode
 letterToEmpty 'd' = Just $ BDict M.empty
@@ -82,9 +48,9 @@ isNum = (`elem` ("0123456789"::String))
 parseInt :: String -> Run BEncode
 parseInt ('-':xs) =
   case parseInt xs of
-    (Run rest (Just (BInteger x))) -> (Run rest (Just (BInteger (negate x))))
+    Run rest (Just (BInteger x)) -> Run rest (Just (BInteger (negate x)))
     returnValue -> returnValue
-parseInt (xs) = Run rest maybeBInteger
+parseInt xs = Run rest maybeBInteger
   where maybeBInteger = fmap (BInteger . fromIntegral) $ charsToMaybeInt $ takeWhile (/= 'e') xs
         rest = dropWhile (== 'e') $ dropWhile (/= 'e') xs
 
@@ -99,12 +65,12 @@ parseString xs =
         probablyInt = charsToMaybeInt $ takeWhile (/= ':') xs
         string i = take i afterNumber
         restOfString i = drop i afterNumber
-        
+
 makeDict :: String -> Maybe ((Run BEncode, Run BEncode), String)
 makeDict ('e':_) = Nothing
 makeDict string = if isNothing maybeBencode2
                     then Nothing
-                    else Just (((Run rest1 maybeBencode1), (Run rest2 maybeBencode2)), rest2)
+                    else Just ((Run rest1 maybeBencode1, Run rest2 maybeBencode2), rest2)
     where (Run rest1 maybeBencode1) = decode string
           (Run rest2 maybeBencode2) = decode rest1
 
@@ -112,60 +78,47 @@ unfoldList :: String -> Maybe (Run BEncode, String)-- -> [Run BEncode]
 unfoldList ('e':_) = Nothing
 unfoldList string = if isNothing maybeBencode
                     then Nothing
-                    else Just ((Run rest maybeBencode), rest)
+                    else Just (Run rest maybeBencode, rest)
     where (Run rest maybeBencode) = decode string
--- NOTE It seems like it would be good to be able to know where the computation failed so that we can fix the file we are parsing.
+
 decode :: String ->  Run BEncode
-decode ('d':xs) = if isNothing maybeRest
-                  then Run ('l':xs) Nothing
-                  else Run (fromJust maybeRest) dict
+decode ('d':xs) =
+  case (unfold, isNothing maybeRest) of
+    ([], _) -> Run "" dict
+    (_, False) -> Run (fromJust maybeRest) dict
+    _          -> Run ('l':xs) Nothing
   where unfold :: [(Run BEncode, Run BEncode)]
         unfold = unfoldr makeDict xs
         dict = if any (\(x, y) -> isNothing x || isNothing y) maybeList
                then Nothing
                else Just (BDict (M.fromList $ fmap (\(x,y) -> (fromJust x, fromJust y)) maybeList))
-        maybeList = fmap (\((Run _ bencode1), (Run _ bencode2)) -> (bencode1, bencode2)) unfold
-        maybeRest =  (maybeTail $ reverse unfold) >>= maybeHead  >>= restToMaybe
+        maybeList = fmap (\(Run _ bencode1, Run _ bencode2) -> (bencode1, bencode2)) unfold
+        maybeRest =  (maybeHead (reverse unfold)) >>= restToMaybe
         restToMaybe :: (Run BEncode, Run BEncode) -> Maybe String
         restToMaybe (_, (Run ('e':rs) _)) = Just rs
         restToMaybe _ = Nothing
-        
-decode ('l':xs) = if isNothing maybeRest
-                  then Run ('l':xs) Nothing
-                  else Run (fromJust maybeRest) list
+
+decode ('l':xs) =
+  case (unfold, isNothing maybeRest) of
+    ([], _) -> Run "" list
+    (_, False) -> Run (fromJust maybeRest) list
+    _ -> Run ('l':xs) Nothing
   where unfold = unfoldr unfoldList xs
         list = fmap BList $ if any isNothing maybeList
                             then Nothing
-                            else (Just (fmap fromJust maybeList))
+                            else Just (fmap fromJust maybeList)
         maybeList = fmap (\(Run _ bencode) -> bencode ) unfold
-        maybeRest =  (maybeHead $ reverse unfold) >>= (\(Run (r:rs) _) -> if r == 'e' then Just rs else Nothing)                  -- case decode xs of
+        maybeRest =  maybeHead (reverse unfold) >>= (\(Run (r:rs) _) -> if r == 'e' then Just rs else Nothing)
 
 decode ('i':xs) = parseInt xs
 decode xs = parseString xs
 
-maybeTail :: [a] -> Maybe [a]
-maybeTail [] = Nothing
-maybeTail (_:xs) = Just xs
-
 maybeHead :: [a] -> Maybe a
 maybeHead [] = Nothing
 maybeHead (x:_) = Just x
-
--- decode :: String -> BEncode ->  RunBEncode
--- decode (xs) (BDict ys) = foldr makeDict (Just (BDict ys)) xs
--- -- decode ('l':xs)
--- -- decode ('i':xs)
--- decode (xs) = 
---   where (string, rest) = getStringAndRest xs
-
---isNum = (`elem` "0123456789")
--- decode :: String -> Maybe BEncode
--- decode xs = snd $ foldr parse ("", xs, BEmpty) xs
-
 
 charsToMaybeInt :: String -> Maybe Int
 charsToMaybeInt = foldl' charsToMaybeIntFold (Just 0)
   where charsToMaybeIntFold :: Maybe Int -> Char -> Maybe  Int
         charsToMaybeIntFold Nothing _ = Nothing
         charsToMaybeIntFold (Just sumInt) char = (+) (sumInt * 10) <$> digitToI char
-        
--- htorrent/test/Spec.hs
+++ htorrent/test/Spec.hs
@@ -6,6 +6,24 @@ import Test.QuickCheck.Classes
 import BEncode
 import Data.Map as M
 
+-- data BEncode = BInteger Integer
+--              | BString String
+--              | BList [BEncode]
+--              | BDict (M.Map BEncode BEncode)
+--              deriving (Eq, Show, Ord)
+instance Arbitrary BEncode where
+  arbitrary = do
+    r <- arbitrary
+    x <- arbitrary
+    y <- arbitrary
+    z <- arbitrary
+    frequency [ (1, return $ BInteger r)
+              , (1, return $ BString x)
+              , (1, return $ BList y)
+              , (1, return $ BDict z)
+              ]
+
+
 charsToMaybeInt_prop :: Positive Int -> Bool
 charsToMaybeInt_prop (Positive xs) = (charsToMaybeInt stringifiedXs) == (Just xs)
   where stringifiedXs = show xs
@@ -15,12 +33,18 @@ main = hspec $ do
   describe "charsToMaybeInt" $ do
     it "follows the property that any number, when stringified is able to be reparsed" $ do
       property charsToMaybeInt_prop
+
   describe "decode" $ do
+    -- describe "round trip property" $ do
+    --   it "should decode any valid encoded BEncode datatype" $ do
     describe "strings" $ do
+      it "can parse an empty string" $ do
+        decode "0:" `shouldBe` Run  "" (Just (BString ""))
       it "returns a BString with no unparsed data if given a well formed string" $ do
         decode "5:hello" `shouldBe` Run  "" (Just (BString "hello"))
       it "returns a BString with no parsed data if given a malformed string" $ do
         decode "5:helloasdf" `shouldBe` Run "asdf" (Just (BString "hello"))
+
     describe "integers" $ do
       it "returns a BInteger with no unparsed data if given a well formed integer" $ do
         decode "i9001e" `shouldBe` Run "" (Just (BInteger 9001))
@@ -28,10 +52,22 @@ main = hspec $ do
         decode "i9001easdf" `shouldBe` Run "asdf" (Just (BInteger 9001))
       it "returns a BInteger with no unparsed data if given a well formed negative integer" $ do
         decode "i-9001e" `shouldBe` Run "" (Just (BInteger (-9001)))
+
     describe "lists" $ do
+      it "can parse an empty lists" $ do
+        decode "le" `shouldBe` Run "" (Just (BList []))
       it "parses l4:spam4:eggse into BList [BString \"spam\", BString \"eggs\"]" $ do
         decode "l4:spam4:eggse" `shouldBe` Run "" (Just (BList [BString "spam", BString "eggs"]))
       it "parses invalid lists to nothing with the rest maintained" $ do
         decode "l4:spam4:eggsasdf" `shouldBe` Run "l4:spam4:eggsasdf" Nothing
+
+    describe "dicts" $ do
+      it "can parse an empty dict" $ do
+        decode "de" `shouldBe` Run "" (Just (BDict $ M.fromList []))
+      it "can parse a dict" $ do
+        decode "d9:publisher3:bob17:publisher-webpage15:www.example.com18:publisher.location4:homee" `shouldBe` Run "" (Just (BDict $ M.fromList $ [(BString "publisher",BString "bob"), (BString "publisher-webpage",BString "www.example.com"),(BString "publisher.location",BString "home")]))
       it "parses d3:cow3:moo4:spam4:eggse into BDict M.map [(\"cow\",\"moo\"), (\"spam\", \"eggs\")]" $ do
         decode "d3:cow3:moo4:spam4:eggse" `shouldBe` Run "" (Just $ BDict $ M.fromList [(BString "cow", BString "moo"), (BString "spam", BString "eggs")])
+      it "parses lists in dicts" $ do
+        decode "d4:spaml1:a1:bee" `shouldBe` Run "" (Just $ BDict $ M.fromList [(BString "spam", BList [BString "a", BString "b"])])
+
